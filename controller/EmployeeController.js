4272e9c778e8d6115f6186f6e2cc95ffa1a18089
samirvithlani <samir.vithlani83955@gmail.com>
2023-12-02 18:33:14 +0530

    app.js changes

--- controller/EmployeeController.js
+++ controller/EmployeeController.js
@@ -1,6 +1,9 @@
 const employeeModel = require("../models/EmployeeModel");
 const encryptPassword = require("../util/PasswordEncrypt");
 const tokenUtil = require("../util/TokenUtil");
+const readDataFromExcel = require("../util/ReadDataFromExcel");
+const multer = require("multer");
+const path = require("path");
 
 const getAllEmployees = async (req, res) => {
   const employees = await employeeModel.find();
@@ -22,13 +25,12 @@ const addEmployee = async (req, res) => {
   console.log("req body", req.body);
   //req.body
 
-  const empData ={
-    name:req.body.name,
-    email:req.body.email,
-    age:req.body.age,
-    password:encryptPassword.encryptPassword(req.body.password)
-  }
-
+  const empData = {
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+    password: encryptPassword.encryptPassword(req.body.password),
+  };
 
   const employee = new employeeModel(empData);
   try {
@@ -120,78 +122,112 @@ const getEmployeeById = async (req, res) => {
   }
 };
 
-const filterEmployee  = async (req,res) => {
+const filterEmployee = async (req, res) => {
+  console.log(req.query);
+  const query = req.query;
+  const data = await employeeModel.find({ ...query });
+  if (data) {
+    res.status(200).json({
+      message: "success",
+      data: data,
+    });
+  } else {
+    res.status(404).json({
+      message: "failed",
+      data: [],
+    });
+  }
+  console.log(query);
+};
 
-        console.log(req.query);
-        const query = req.query;
-        const data = await employeeModel.find({...query});
-        if(data){
-            res.status(200).json({
-                message:"success",
-                data:data
-            })
-        }else{
-            res.status(404).json({
-                message:"failed",
-                data:[]
-            })
-        }
-        console.log(query);
+const loginEmployee = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(email, password);
 
-
-}
-
-const loginEmployee = async (req,res) => {
-
-
-    const email = req.body.email;
-    const password = req.body.password;
-    console.log(email,password);
-
-    const employee = await employeeModel.findOne({email:email});
-    console.log("employee",employee);
-    if(employee){
-
-      const flag = encryptPassword.comparePassword(password,employee.password);
-      console.log(flag);
-      if(flag){
-
-        const token = tokenUtil.generateToken(employee.toObject())
-        console.log("token...",token);
-        res.status(200).json({
-          message:"Login success",
-          data:token
-        })
-      }
-      else{
-        res.status(404).json({
-          message:"Login failed",
-          data:[]
-        })
-      }
-
-    }
-    else{
-
+  const employee = await employeeModel.findOne({ email: email });
+  console.log("employee", employee);
+  if (employee) {
+    const flag = encryptPassword.comparePassword(password, employee.password);
+    console.log(flag);
+    if (flag) {
+      const token = tokenUtil.generateToken(employee.toObject());
+      console.log("token...", token);
+      res.status(200).json({
+        message: "Login success",
+        data: token,
+      });
+    } else {
       res.status(404).json({
-        message:"Register first",
-        data:[]
-      })
-
+        message: "Login failed",
+        data: [],
+      });
     }
+  } else {
+    res.status(404).json({
+      message: "Register first",
+      data: [],
+    });
+  }
+};
 
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  // fileFilter: (req, file, cb) => {
+  //   if (
+  //     file.mimetype == "image/png" ||
+  //     file.mimetype == "image/jpg" ||
+  //     file.mimetype == "image/jpeg"
+  //   ) {
+  //       cb(null, true);
 
-}
+  //   } else {
+  //     cb(null, false);
+  //     return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+  //   }
+  // },
+}).single("file");
 
+const addBulkEmployee = async (req, res) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    } else {
+      console.log(req.file.path);
+      const data = readDataFromExcel.readDataFromExcel(req.file.path);
 
+      const flag = await employeeModel.insertMany(data);
+      if (flag) {
+        res.status(200).json({
+          message: "success",
+          data: flag,
+        });
+      } else {
+        res.status(200).json({
+          message: "failed",
+        });
+      }
+    }
+  });
+};
 
 module.exports = {
   getAllEmployees,
   addEmployee,
   deleteEmployee,
   updateEmployee,
-    getEmployeeById,
-    filterEmployee,
-    loginEmployee
+  getEmployeeById,
+  filterEmployee,
+  loginEmployee,
+  addBulkEmployee,
 };
--- routes/EmployeeRoutes.js
+++ routes/EmployeeRoutes.js
@@ -15,5 +15,5 @@ router.put("/employee/:id",employeeController.updateEmployee);
 router.get("/employee/:id",employeeController.getEmployeeById);
 router.get("/employee1/filter",employeeController.filterEmployee);
 router.post("/employee/login",employeeController.loginEmployee);
-
+router.post("/employee/bulk",employeeController.addBulkEmployee);
 module.exports = router;
